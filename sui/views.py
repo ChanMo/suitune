9e6430dccb9e95dd65a7ef5bab51ec9b65d6245b
ChanMo <chan.mo@outlook.com>
2025-08-06 23:48:59 +0800

    feat: 添加基础模板和首页

--- sui/views.py
+++ sui/views.py
@@ -1,8 +1,13 @@
+from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Track
 from .serializers import TrackSerializer
+
+
+def home(request):
+    return render(request, "sui/index.html")
 
 
 class NextTrackView(APIView):
